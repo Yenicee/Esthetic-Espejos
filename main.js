9e169f1185649a707f3c8541b5b5ac4b9ce5e507
Yenice <perezyenice@gmail.com>
2023-06-23 15:28:39 -0300

    informacion a paginas

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
-//ACA SE GENERO EL CARRITO DE LA PAGINA
-const btnCart = document.querySelector('.container-cart-icon')
-const containerCartProductos = document.querySelector('.container-cart-productos')
+// //ACA SE GENERO EL CARRITO DE LA PAGINA
+// const btnCart = document.querySelector('.container-cart-icon')
+// const containerCartProductos = document.querySelector('.container-cart-productos')
 
-btnCart.addEventListener('click', () => {
-    containerCartProductos.classList.toggle('hidden-cart')
+// btnCart.addEventListener('click', () => {
+//     containerCartProductos.classList.toggle('hidden-cart')
 
-})
+// })
 
-/***********************************************/
-const cartInfo = document.querySelector('.cart-product')
-const rowProduct = document.querySelector('.row-product')
+// /***********************************************/
+// const cartInfo = document.querySelector('.cart-product')
+// const rowProduct = document.querySelector('.row-product')
 
-//lista de todos los contenedores de productos
-const productsList = document.querySelector('container-item')
+// //lista de todos los contenedores de productos
+// const productsList = document.querySelector('container-item')
 
-//VARIABLE DE ARREGLOS DE PRODUCTOS
-let allProducts = []
-
-
+// //VARIABLE DE ARREGLOS DE PRODUCTOS
+// let allProducts = []
 
 
 
-productsList.addEventListener('click' , e => {
+// productsList.addEventListener('click' , e => {
 
-            console.log(e.target.classList)
+//             console.log(e.target.classList)
 
-})
+// })
 
-
-
+//poner un alert de bienvenida a la pagina 
+Swal.fire(
+     {
+      title: 'Solo por hoy cupos limitados',
+      text: 'En depilacion definitiva y un 10% descuento en productos',
+      //da los segundos y desaparece solo
+      timer: '5000',
+      background: '#d0bff7b8',
+      color:'white',
+      position: 'bottom-end',
+      confirmButtonColor:'#4f1ec0b8',
+   }
+)
 
 
 
