409cc6bc60b5345a35140e86f2cd0c71ab851a2f
Yenice <perezyenice@gmail.com>
2023-06-24 19:42:07 -0300

    solucion de cart

--- main.js
+++ main.js
@@ -1,43 +1,135 @@
-// //ACA SE GENERO EL CARRITO DE LA PAGINA
-// const btnCart = document.querySelector('.container-cart-icon')
-// const containerCartProductos = document.querySelector('.container-cart-productos')
+const btnCart = document.querySelector('.container-icon');
+const containerCartProducts = document.querySelector('.container-cart-products');
 
-// btnCart.addEventListener('click', () => {
-//     containerCartProductos.classList.toggle('hidden-cart')
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart');
+});
 
-// })
+/* ========================= */
+const rowProduct = document.querySelector('.row-product');
 
-// /***********************************************/
-// const cartInfo = document.querySelector('.cart-product')
-// const rowProduct = document.querySelector('.row-product')
+// Variable de arreglo de Productos
+let allProducts = [];
 
-// //lista de todos los contenedores de productos
-// const productsList = document.querySelector('container-item')
+const valorTotal = document.querySelector('.total-pagar');
 
-// //VARIABLE DE ARREGLOS DE PRODUCTOS
-// let allProducts = []
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+const addCartButtons = document.querySelectorAll('.btn-add-cart');
+
+addCartButtons.forEach(button => {
+    button.addEventListener('click', e => {
+        const product = e.target.parentElement;
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h2').textContent,
+            price: product.querySelector('p').textContent,
+        };
+
+        const exists = allProducts.some(product => product.title === infoProduct.title);
+
+        if (exists) {
+            const products = allProducts.map(product => {
+                if (product.title === infoProduct.title) {
+                    product.quantity++;
+                    return product;
+                } else {
+                    return product;
+                }
+            });
+            allProducts = [...products];
+        } else {
+            allProducts = [...allProducts, infoProduct];
+        }
+
+        showHTML();
+    });
+});
+
+rowProduct.addEventListener('click', e => {
+    if (e.target.classList.contains('icon-close')) {
+        const product = e.target.parentElement;
+        const title = product.querySelector('p').textContent;
+
+        allProducts = allProducts.filter(product => product.title !== title);
+
+        showHTML();
+    }
+});
+
+// Función para mostrar HTML
+const showHTML = () => {
+    if (!allProducts.length) {
+        cartEmpty.classList.remove('hidden');
+        rowProduct.classList.add('hidden');
+        cartTotal.classList.add('hidden');
+    } else {
+        cartEmpty.classList.add('hidden');
+        rowProduct.classList.remove('hidden');
+        cartTotal.classList.remove('hidden');
+    }
+
+    // Limpiar HTML
+    rowProduct.innerHTML = '';
+
+    let total = 0;
+    let totalOfProducts = 0;
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('cart-product');
+
+        containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+        rowProduct.append(containerProduct);
+
+        total += parseInt(product.quantity * product.price.slice(1));
+        totalOfProducts += product.quantity;
+    });
+
+    valorTotal.innerText = `$${total}`;
+    countProducts.innerText = totalOfProducts;
+};
+
+showHTML();
 
 
-
-// productsList.addEventListener('click' , e => {
-
-//             console.log(e.target.classList)
-
-// })
-
-//poner un alert de bienvenida a la pagina 
-Swal.fire(
-     {
-      title: 'Solo por hoy cupos limitados',
-      text: 'En depilacion definitiva y un 10% descuento en productos',
-      //da los segundos y desaparece solo
-      timer: '5000',
-      background: '#d0bff7b8',
-      color:'white',
-      position: 'bottom-end',
-      confirmButtonColor:'#4f1ec0b8',
-   }
-)
+// Swal.fire(
+//     {
+//      title: 'Solo por hoy cupos limitados',
+//      text: 'En depilacion definitiva y un 10% descuento en productos',
+//      //da los segundos y desaparece solo
+//      timer: '5000',
+//      background: '#d0bff7b8',
+//      color:'white',
+//      position: 'bottom-end',
+//      confirmButtonColor:'#4f1ec0b8',
+//   }
+// ) 
 
 
 
