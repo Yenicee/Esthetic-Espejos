511881acd568233873ee180bdde6a01c4f2db49a
Yenice <perezyenice@gmail.com>
2023-10-14 22:24:40 -0300

    nuevas paginas

--- main.js
+++ main.js
@@ -1,53 +1,51 @@
  //SCROLL DE PRODUCTOS DE LA PAG INICIO
 
- document.addEventListener("DOMContentLoaded", function () {
-    const sliderContainer = document.querySelector(".slider-container");
-    let isDragging = false;
-    let startPosition = 0;
-    let currentTranslate = 0;
-    let prevTranslate = 0;
-  
-    function startDrag(event) {
-      event.preventDefault();
-      isDragging = true;
-      startPosition = getPositionX(event);
-      sliderContainer.style.cursor = "grabbing";
-      requestAnimationFrame(() => updateSlider(event)); // Pasamos el evento a la función updateSlider
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderContainer = document.querySelector(".slider-container");
+  let isDragging = false;
+  let startPosition = 0;
+  let currentTranslate = 0;
+  let prevTranslate = 0;
+
+  const startDrag = (event) => {
+    event.preventDefault();
+    isDragging = true;
+    startPosition = getPositionX(event);
+    sliderContainer.style.cursor = "grabbing";
+    requestAnimationFrame(() => updateSlider(event));
+  };
+
+  const updateSlider = (event) => {
+    if (isDragging) {
+      const currentPosition = getPositionX(event);
+      const diffX = currentPosition - startPosition;
+      currentTranslate = prevTranslate + diffX;
+      setSliderPosition();
+      requestAnimationFrame(() => updateSlider(event));
     }
-  
-    function updateSlider(event) {
-      if (isDragging) {
-        const currentPosition = getPositionX(event);
-        const diffX = currentPosition - startPosition;
-        currentTranslate = prevTranslate + diffX;
-        setSliderPosition();
-        requestAnimationFrame(() => updateSlider(event));
-      }
-    }
-  
-    function endDrag() {
-      isDragging = false;
-      prevTranslate = currentTranslate;
-      sliderContainer.style.cursor = "grab";
-    }
-  
-    function getPositionX(event) {
-      // Verificamos si es un evento de toque en dispositivos táctiles
-      return event.type.includes("touch") ? event.touches[0].clientX : event.clientX;
-    }
-  
-    function setSliderPosition() {
-      sliderContainer.style.transform = `translateX(${currentTranslate}px)`;
-    }
-  
+  };
+
+  const endDrag = () => {
+    isDragging = false;
+    prevTranslate = currentTranslate;
+    sliderContainer.style.cursor = "grab";
+  };
+
+  const getPositionX = (event) => {
+    return event.type.includes("touch") ? event.touches[0].clientX : event.clientX;
+  };
+
+  const setSliderPosition = () => {
+    sliderContainer.style.transform = `translateX(${currentTranslate}px)`;
+  };
+
   sliderContainer.addEventListener("mousedown", startDrag);
-   sliderContainer.addEventListener("mousemove", updateSlider);
-   sliderContainer.addEventListener("mouseup", endDrag);
-   sliderContainer.addEventListener("mouseleave", endDrag);
-  
-   // Permite el desplazamiento en pantallas táctiles
-    sliderContainer.addEventListener("touchstart", startDrag);
-    sliderContainer.addEventListener("touchmove", updateSlider);
-    sliderContainer.addEventListener("touchend", endDrag);
-    sliderContainer.addEventListener("touchcancel", endDrag);
-  });
+  sliderContainer.addEventListener("mousemove", updateSlider);
+  sliderContainer.addEventListener("mouseup", endDrag);
+  sliderContainer.addEventListener("mouseleave", endDrag);
+
+  sliderContainer.addEventListener("touchstart", startDrag);
+  sliderContainer.addEventListener("touchmove", updateSlider);
+  sliderContainer.addEventListener("touchend", endDrag);
+  sliderContainer.addEventListener("touchcancel", endDrag);
+});
